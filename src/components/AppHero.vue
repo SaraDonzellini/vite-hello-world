8c28e344609c0e290b3fe2d8a34e505a72efe6a1
SaraDonzellini <saradonzie@gmail.com>
2022-12-23 14:21:31 +0100

    added title + jumbo image

---
+++ src/components/AppHero.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <section class="d-flex">
+    <img src="../assets/jumbo.bmp" alt="jumbotron  vite + vue" class="jumbo">
+  </section>
+</template>
+
+<style scoped>
+
+.jumbo{
+  text-align: center;
+  margin: 0 auto;
+}
+
+.d-flex {
+  display: flex;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <section>
+    <h1>
+      La mia prima app con Vite!
+    </h1>
+  </section>
+</template>
+
+<style scoped>
+
+h1{
+  text-decoration: underline;
+  text-align: center;
+  color: white;
+}
+
+</style>
